4f7459ec57fa9535bbd67bf0f01ab7c8e2dbab31
ClarXo <imp_rove@naver.com>
2021-12-17 17:55:17 +0900

    Quest4 - icon & folder & desktop
    todo
    - window

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,15 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Desktop(new Icon(3), new Folder(2));
+});
+
+class Draggable {
+  add(element) {
+    document.createElement(element);
+  }
+}
+
+class File {
+  constructor(count) {
+    this.count = count;
+  }
+  add = (html) => {
+    let innerHtml = "";
+    new Array(this.count).fill().forEach(() => (innerHtml += html));
+    return innerHtml;
+  };
+}
+
 class Desktop {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+  #isDragging = false;
+  #desktop = document.createElement("section");
+  constructor(icon, folder) {
+    this.#initDesktop(icon, folder);
+    this.#registHandlers();
+  }
+  #initDesktop = (icon, folder) => {
+    this.#desktop.classList.add("desktop");
+    this.#desktop.insertAdjacentHTML(
+      "beforeend",
+      `<ul class="file-wrapper">
+				${icon.addIcon()}
+				${folder.addFolder()}
+			</ul>`
+    );
+    document.body.append(this.#desktop);
+  };
+  #registHandlers = () => {
+    this.#desktop.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      const target = e.target;
+      if (!this.#isDragging && target.classList.contains("file")) {
+        this.#isDragging = true;
+        target.classList.add("onDrag");
+      }
+    });
+    this.#desktop.addEventListener("mouseup", (e) => {
+      if (this.#isDragging) {
+        console.log(e.clientX);
+        console.log("mouseup");
+        const fileOnDrag = this.#desktop.querySelector(".onDrag");
+        this.#isDragging = false;
+        fileOnDrag.classList.add("dragged");
+        console.log(fileOnDrag.style.height);
+        fileOnDrag.style.top = `${e.clientY - 32}px`;
+        fileOnDrag.style.left = `${e.clientX - 32}px`;
+        console.log(fileOnDrag.style);
+        fileOnDrag.classList.remove("onDrag");
+      }
+    });
+  };
+}
 
-class Icon {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+class Icon extends File {
+  constructor(count) {
+    super(count);
+  }
+  addIcon = () => this.add("<li class='file icon'></li>");
+}
 
-class Folder {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+class Folder extends File {
+  constructor(count) {
+    super(count);
+  }
+  addFolder = () => this.add("<li class='file folder'></li>");
+}
 
-class Window {
-	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+class Window {}
