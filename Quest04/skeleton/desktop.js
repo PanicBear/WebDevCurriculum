ece200106ca53377cb6c343cdca02be7256fded6
ClarXo <imp_rove@naver.com>
2021-12-20 16:53:06 +0900

    Q5 - dblclick

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  new Desktop(new Icon(3), new Folder(2));
+  new Desktop(new Icon(3), new Folder(2), new Window());
+  new Desktop(new Icon(3), new Folder(2), new Window());
 });
 
 class Draggable {
@@ -8,7 +9,7 @@ class Draggable {
   }
 }
 
-class File {
+class GUI {
   constructor(count) {
     this.count = count;
   }
@@ -22,7 +23,12 @@ class File {
 class Desktop {
   #isDragging = false;
   #desktop = document.createElement("section");
-  constructor(icon, folder) {
+  #target = null;
+  #ghost = null;
+  #window = null;
+
+  constructor(icon, folder, win) {
+    this.#window = win;
     this.#initDesktop(icon, folder);
     this.#registHandlers();
   }
@@ -41,40 +47,81 @@ class Desktop {
     this.#desktop.addEventListener("mousedown", (e) => {
       e.preventDefault();
       const target = e.target;
-      if (!this.#isDragging && target.classList.contains("file")) {
+      if (!this.#isDragging && target.classList.contains("draggable")) {
         this.#isDragging = true;
         target.classList.add("onDrag");
+        this.#target = target;
+        this.#addGhost();
       }
     });
     this.#desktop.addEventListener("mouseup", (e) => {
       if (this.#isDragging) {
-        console.log(e.clientX);
-        console.log("mouseup");
         const fileOnDrag = this.#desktop.querySelector(".onDrag");
         this.#isDragging = false;
         fileOnDrag.classList.add("dragged");
-        console.log(fileOnDrag.style.height);
-        fileOnDrag.style.top = `${e.clientY - 32}px`;
-        fileOnDrag.style.left = `${e.clientX - 32}px`;
-        console.log(fileOnDrag.style);
+        fileOnDrag.style.top = `${e.pageY - 32}px`;
+        fileOnDrag.style.left = `${e.pageX - 32}px`;
         fileOnDrag.classList.remove("onDrag");
+        this.#target = null;
+        this.#desktop.removeChild(this.#ghost);
+        this.#ghost = null;
+      }
+    });
+    this.#desktop.addEventListener("dblclick", () => {
+      this.#window.addWindow(this.#desktop);
+    });
+    this.#desktop.addEventListener("mousemove", (e) => {
+      if (this.#isDragging && this.#ghost) {
+        this.#ghost.style.top = `${e.pageY - 32}px`;
+        this.#ghost.style.left = `${e.pageX - 32}px`;
+        this.#ghost.style.color = "black";
       }
     });
   };
+  #addGhost = () => {
+    if (this.#target.classList.contains("fa-file")) {
+      this.#ghost = document.createElement("i");
+      this.#ghost.className = "far fa-file ghost";
+    } else if (this.#target.classList.contains("fa-folder")) {
+      this.#ghost = document.createElement("i");
+      this.#ghost.className = "far fa-folder ghost";
+    } else if (this.#target.classList.contains("win")) {
+      this.#ghost = document.createElement("section");
+      this.#ghost.className = "win ghost";
+    }
+    this.#desktop.insertAdjacentElement("beforeend", this.#ghost);
+  };
 }
 
-class Icon extends File {
+class Icon extends GUI {
   constructor(count) {
     super(count);
   }
-  addIcon = () => this.add("<li class='file icon'></li>");
+  addIcon = () =>
+    this.add(
+      "<li class='file icon'><i class='far fa-file draggable'></i></li>"
+    );
 }
 
-class Folder extends File {
+class Folder extends GUI {
   constructor(count) {
     super(count);
   }
-  addFolder = () => this.add("<li class='file folder'></li>");
+  addFolder = () =>
+    this.add(
+      "<li class='file folder'><i class='far fa-folder draggable'></i></li>"
+    );
 }
 
-class Window {}
+class Window extends GUI {
+  constructor() {
+    super(1);
+  }
+  addWindow = (target) => {
+    console.log("addWindow");
+    target.insertAdjacentHTML(
+      "beforeEnd",
+      this.add("<i class='win far fa-window-maximize draggable'></i>")
+    );
+  };
+}
