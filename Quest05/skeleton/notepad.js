c163daaa30907ff1422c4dfb75a112a8353f3665
ClarXo <imp_rove@naver.com>
2021-12-21 17:54:18 +0900

    Q5 - add file

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -1,3 +1,102 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const explorer = new Explorer(document.querySelector(".file-list"));
+  const notepad = new Notepad(
+    document.querySelector("#root"),
+    explorer,
+    new File()
+  );
+});
+
 class Notepad {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-};
+  #notepad;
+  #explorer;
+  #file;
+  #onEditList = {};
+  constructor(target, explorer, file) {
+    this.#notepad = target;
+    this.#explorer = explorer;
+    this.#file = file;
+    this.#initEventHandler();
+  }
+  #initEventHandler = () => {
+    this.#notepad.addEventListener("click", (e) => {
+      const target = e.target;
+      if (target.classList.contains("add-file")) {
+        this.#explorer.addFile();
+      } else if (target.classList.contains("edit-file")) {
+        this.#editFile();
+      } else if (target.classList.contains("delete-file")) {
+        this.#deleteFile();
+      }
+    });
+    this.#notepad.addEventListener("dblclick", (e) => {
+      const target = e.target;
+      if (target.classList.contains("item-text")) {
+        const file = localStorage.getItem("files")[target.textContent];
+        if (this.#onEditList[target.textContent]) {
+          this.#onEditList[file.name] = file;
+        }
+      }
+    });
+  };
+  #editFile = () => {
+    console.log("edit file");
+  };
+  #deleteFile = () => {
+    console.log("delete file");
+  };
+}
+
+class Explorer {
+  #storage;
+  #root;
+  constructor(fileList) {
+    this.#root = fileList;
+    this.#initStorage();
+  }
+  #initStorage = () => {
+    this.#storage = localStorage;
+    this.#storage.getItem("files") ??
+      this.#storage.setItem("files", JSON.stringify({}));
+    this.#onChange();
+  };
+  addFile = () => {
+    const files = JSON.parse(this.#storage.getItem("files"));
+    const name = prompt("생성할 파일의 이름을 입력하세요");
+    if (name) {
+      files[name] = { name, content: "" };
+      this.#storage.setItem("files", JSON.stringify(files));
+      this.#onChange();
+    }
+  };
+  #onChange = () => {
+    console.log("onchange");
+    const files = JSON.parse(this.#storage.getItem("files"));
+    const items = Object.keys(files).map(
+      (fileName) =>
+        `<li class="file-item">
+		<div class="item-text">${fileName}</div>
+		<div class="item-button">
+			<i class="far fa-edit edit-file"></i>
+			<i class="far fa-trash-alt delete-file"></i>
+		</div>
+	</li>`
+    );
+    this.#root.innerHTML = items.join("");
+  };
+}
+
+class File {
+  #title = "";
+  #content = "";
+  updateFile(title, content) {
+    this.#title = title;
+    this.#content = content;
+  }
+  getFile = () => {
+    return {
+      title: this.#title,
+      content: this.#content,
+    };
+  };
+}
